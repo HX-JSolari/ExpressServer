1f6abdd7aa9f795548b8e0aa7138dc91968e656e
JJSolari <jignaciosolari@gmail.com>
2021-09-02 14:10:35 -0300

    ExpressSv

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var routes = require('./index.js')
+module.exports = app; // esto es solo para testear mas facil
+
+// acuerdense de agregar su router o cualquier middleware que necesiten aca
+app.use(express.json())
+
+
+
+// el condicional es solo para evitar algun problema de tipo EADDRINUSE con mocha watch + supertest + npm test.
+if (!module.parent) app.listen(3000);
---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+router.use(express.json());
+
+var tareas = []
+
+// escriban sus rutas acá 
+// siéntanse libres de dividir entre archivos si lo necesitan
+
+
+
+module.exports = {
+    router, 
+    tareas
+};
